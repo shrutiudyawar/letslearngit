312edd8843a1ea0380ec28d2e9191603a160e80b
shrutiudyawar <shrutiudyawar@gmail.com>
2021-07-15 17:56:16 +0530

    Changes done to sum.java file in third commit

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class Sum{
 	public static void main(String[] a){
 		int num1 = 10,num = 20,sum;
 		sum = num1+num;
-		System.out.println("Sum of two numbers is: "+sum);
+		System.out.println("Sum of two numbers" + num1 + "and" + num+ "is: "+sum);
 	}
 }
 
